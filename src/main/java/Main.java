efad5428b2e0fcca4afa36eff60f5125542951f8
jjheffernan <jjheffernan99@gmail.com>
2022-06-28 23:16:43 -0400

    completed SumOfInput, uses local scanner within main, and for loop that goes to input number. Large commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
+
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input number: ");
+        int input = scan.nextInt();
+        for (int i = 0; i <= input; i++) {
+            System.out.println(i);
+        }
+
 
     }
 }
